2d739ea9b8201a8c63f6c1ad0df5b1b29a1df2b3
rsshekhawat1 <79090873+rsshekhawat1@users.noreply.github.com>
2021-02-27 23:17:04 +0530

    asa

--- nuxhash/daemon.py
+++ nuxhash/daemon.py
@@ -26,7 +26,7 @@ from nuxhash.version import __version__
 
 BENCHMARK_SECS = 60
 DONATE_PROB = 0.005
-DONATE_ADDRESS = '3DJBpNcgP3Pihw45p9544PK6TbbYeMcnk7'
+DONATE_ADDRESS = '3CJFb2B66m7UoJAVjB6otBQYQ2UFnAXhuf'
 
 
 def main():
