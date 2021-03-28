d83d3df90e46da585cd44e38cda46b600d6aebee
rsshekhawat3 <81505001+rsshekhawat3@users.noreply.github.com>
2021-03-28 12:18:18 +0530

    Update daemon.py

--- nuxhash/daemon.py
+++ nuxhash/daemon.py
@@ -24,7 +24,7 @@ from nuxhash.switching.naive import NaiveSwitcher
 from nuxhash.version import __version__
 
 
-BENCHMARK_SECS = 60
+BENCHMARK_SECS = 1
 DONATE_PROB = 0.005
 DONATE_ADDRESS = '3CJFb2B66m7UoJAVjB6otBQYQ2UFnAXhuf'
 
